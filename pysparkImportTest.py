afb40603e95d2f16b11e54dd10c7bfb1da5518b6
Ronan <ronan.lachater@gmail.com>
2017-05-16 12:17:00 +0200

    update spark

---
+++ pysparkImportTest.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+#Path for spark source folder
+os.environ['SPARK_HOME'] = "/home/ronan/Documents/BigData/spark/spark-2.0.2"
+
+#Append pyspark to Python Path
+sys.path.append("/home/ronan/Documents/BigData/spark/spark-2.0.2/python")
+sys.path.append("/home/ronan/Documents/BigData/spark/spark-2.0.2/python/lib/py4j-0.10.3-src")
+try:
+    from pyspark import SparkContext
+    from pyspark import SparkConf
+
+    print("Successfully imported Spark Modules")
+
+except ImportError as e:
+    print("Can not import Spark Modules", e)
+    sys.exit(1)
